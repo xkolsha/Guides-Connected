1cefd04be80d20e02bf3973dba95551d36298d9f
xkolsha <aviad85@outlook.com>
2023-12-03 19:01:34 -0400

    updated dashboard

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,47 +1,62 @@
 import { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
-import { GET_EXPERTS, GET_CATEGORIES } from "../queries/adminQueries.jsx";
 import {
-  DELETE_EXPERT,
-  DELETE_CATEGORY,
+  GET_EXPERTS,
+  GET_CATEGORIES,
+  ADD_EXPERT,
   UPDATE_EXPERT,
+  DELETE_EXPERT,
+  ADD_CATEGORY,
   UPDATE_CATEGORY,
-} from "../mutations/adminMutations.jsx";
+  DELETE_CATEGORY,
+} from "../queries/adminQueries.jsx";
 
 const AdminDashboard = () => {
+  // State for handling UI and form data
   const [selectedItem, setSelectedItem] = useState(null);
-  const [editMode, setEditMode] = useState(false);
-  const {
-    data: dataExperts,
-    loading: loadingExperts,
-    error: errorExperts,
-  } = useQuery(GET_EXPERTS);
-  const {
-    data: dataCategories,
-    loading: loadingCategories,
-    error: errorCategories,
-  } = useQuery(GET_CATEGORIES);
+  const [formMode, setFormMode] = useState("add"); // 'add' or 'edit'
+  const [formData, setFormData] = useState({
+    name: "",
+    title: "",
+    description: "",
+  });
+  const [currentEditingType, setCurrentEditingType] = useState("expert"); // 'expert' or 'category'
 
+  // Query and Mutation Hooks
+  const { data: expertsData, refetch: refetchExperts } = useQuery(GET_EXPERTS);
+  const { data: categoriesData, refetch: refetchCategories } =
+    useQuery(GET_CATEGORIES);
+
+  const [addExpert] = useMutation(ADD_EXPERT, {
+    onCompleted: () => refetchExperts(),
+  });
   const [updateExpert] = useMutation(UPDATE_EXPERT, {
-    refetchQueries: [{ query: GET_EXPERTS }],
+    onCompleted: () => refetchExperts(),
+  });
+  const [deleteExpert] = useMutation(DELETE_EXPERT, {
+    onCompleted: () => refetchExperts(),
+  });
+
+  const [addCategory] = useMutation(ADD_CATEGORY, {
+    onCompleted: () => refetchCategories(),
   });
   const [updateCategory] = useMutation(UPDATE_CATEGORY, {
-    refetchQueries: [{ query: GET_CATEGORIES }],
-  });
-
-  const [deleteExpert] = useMutation(DELETE_EXPERT, {
-    refetchQueries: [{ query: GET_EXPERTS }],
+    onCompleted: () => refetchCategories(),
   });
   const [deleteCategory] = useMutation(DELETE_CATEGORY, {
-    refetchQueries: [{ query: GET_CATEGORIES }],
+    onCompleted: () => refetchCategories(),
   });
 
-  if (loadingExperts || loadingCategories) return <p>Loading...</p>;
-  if (errorExperts || errorCategories) return <p>Error :(</p>;
-
+  // Handler functions for Edit and Delete
   const handleEditClick = (item, type) => {
-    setSelectedItem({ ...item, type });
-    setEditMode(true);
+    setSelectedItem(item);
+    setFormMode("edit");
+    setCurrentEditingType(type);
+    setFormData({
+      name: item.name,
+      title: item.title || "",
+      description: item.description || "",
+    });
   };
 
   const handleDeleteClick = async (id, type) => {
@@ -52,82 +67,124 @@ const AdminDashboard = () => {
     }
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const formProps = Object.fromEntries(formData);
-
-    if (selectedItem.type === "expert") {
-      await updateExpert({
-        variables: { id: selectedItem._id, expertData: formProps },
-      });
-    } else if (selectedItem.type === "category") {
-      await updateCategory({
-        variables: { id: selectedItem._id, categoryData: formProps },
-      });
-    }
-
-    setEditMode(false);
+  const handleAddNewClick = (type) => {
     setSelectedItem(null);
+    setFormMode("add");
+    setCurrentEditingType(type);
+    setFormData({ name: "", title: "", description: "" });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (currentEditingType === "expert") {
+        const expertData = { name: formData.name, title: formData.title };
+        if (formMode === "add") {
+          await addExpert({ variables: { expertData } });
+        } else {
+          await updateExpert({
+            variables: { id: selectedItem._id, expertData },
+          });
+        }
+      } else if (currentEditingType === "category") {
+        const categoryData = {
+          name: formData.name,
+          description: formData.description,
+        };
+        if (formMode === "add") {
+          await addCategory({ variables: { categoryData } });
+        } else {
+          await updateCategory({
+            variables: { id: selectedItem._id, categoryData },
+          });
+        }
+      }
+      setSelectedItem(null);
+      setFormData({ name: "", title: "", description: "" });
+      setFormMode("add");
+    } catch (error) {
+      console.error("Error handling form submission:", error);
+    }
+  };
+
+  // Render UI components
   return (
     <div>
       <h1>Admin Dashboard</h1>
-      <h2>Experts</h2>
-      {dataExperts.getExperts.map((expert) => (
-        <div key={expert._id}>
-          {expert.name}
-          <button onClick={() => handleEditClick(expert, "expert")}>
-            Edit
-          </button>
-          <button onClick={() => handleDeleteClick(expert._id, "expert")}>
-            Delete
-          </button>
-        </div>
-      ))}
+      {/* Experts Section */}
+      <div>
+        <h2>Experts</h2>
+        {expertsData?.getExperts.map((expert) => (
+          <div key={expert._id}>
+            {expert.name} - {expert.title}
+            <button onClick={() => handleEditClick(expert, "expert")}>
+              Edit
+            </button>
+            <button onClick={() => handleDeleteClick(expert._id, "expert")}>
+              Delete
+            </button>
+          </div>
+        ))}
+        <button onClick={() => handleAddNewClick("expert")}>Add Expert</button>
+      </div>
 
-      <h2>Categories</h2>
-      {dataCategories.getCategories.map((category) => (
-        <div key={category._id}>
-          {category.name}
-          <button onClick={() => handleEditClick(category, "category")}>
-            Edit
-          </button>
-          <button onClick={() => handleDeleteClick(category._id, "category")}>
-            Delete
-          </button>
-        </div>
-      ))}
+      {/* Categories Section */}
+      <div>
+        <h2>Categories</h2>
+        {categoriesData?.getCategories.map((category) => (
+          <div key={category._id}>
+            {category.name}
+            <button onClick={() => handleEditClick(category, "category")}>
+              Edit
+            </button>
+            <button onClick={() => handleDeleteClick(category._id, "category")}>
+              Delete
+            </button>
+          </div>
+        ))}
+        <button onClick={() => handleAddNewClick("category")}>
+          Add Category
+        </button>
+      </div>
 
-      {editMode && (
+      {/* Form for Adding/Editing Experts or Categories */}
+      <div>
+        <h3>
+          {formMode === "add"
+            ? `Add New ${currentEditingType}`
+            : `Edit ${currentEditingType}`}
+        </h3>
         <form onSubmit={handleSubmit}>
-          <h3>Edit {selectedItem.type}</h3>
-          <label>
-            Name:
+          <div>
+            <label>Name:</label>
             <input
               type="text"
-              name="name"
-              defaultValue={selectedItem.name || ""}
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
               required
             />
-          </label>
-          {selectedItem.type === "expert" && (
-            <label>
-              Title:
+          </div>
+          {currentEditingType === "expert" && (
+            <div>
+              <label>Title:</label>
               <input
                 type="text"
-                name="title"
-                defaultValue={selectedItem.title || ""}
+                value={formData.title}
+                onChange={(e) =>
+                  setFormData({ ...formData, title: e.target.value })
+                }
               />
-            </label>
+            </div>
           )}
-          <button type="submit">Save</button>
-          <button type="button" onClick={() => setEditMode(false)}>
-            Cancel
-          </button>
+          <div>
+            <button type="submit">
+              {formMode === "add" ? "Add" : "Update"} {currentEditingType}
+            </button>
+          </div>
         </form>
-      )}
+      </div>
     </div>
   );
 };
