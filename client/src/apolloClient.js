d5891ef0d3e7ebd34de1e9103250d0d3ef790e97
xkolsha <aviad85@outlook.com>
2023-12-08 23:48:07 -0400

    update appolo for deploying

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,8 +1,15 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
+// Use a different URI for production
+const graphqlUri =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_GRAPHQL_URI // Use the production URI in production
+    : "http://localhost:4000/graphql"; // Use localhost in development
+
+// Create a new Apollo client
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
+    uri: graphqlUri,
   }),
   cache: new InMemoryCache(),
 });
