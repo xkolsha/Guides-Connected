e3b0944085a9d4b97fbfe2837dd6f1fa83428d95
xkolsha <aviad85@outlook.com>
2023-12-09 02:48:54 -0400

    pdated apollo

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -3,7 +3,10 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 // purpose of this file is to create a new ApolloClient instance and export it so that we can use it in our app
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000/graphql", // this is the endpoint that we are making requests to
+    uri:
+      process.env.NODE_ENV === "production"
+        ? "https://guides-connected-5d7602b52f90.herokuapp.com/graphql"
+        : "http://localhost:4000/graphql",
   }),
   cache: new InMemoryCache(),
 });
