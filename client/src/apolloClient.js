b231b796641f4729f75e5c0efb84731cce371520
xkolsha <aviad85@outlook.com>
2023-12-09 02:04:07 -0400

    update for heroku

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,15 +1,9 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-// Use a different URI for production
-const graphqlUri =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_GRAPHQL_URI // Use the production URI in production
-    : "http://localhost:4000/graphql"; // Use localhost in development
-
-// Create a new Apollo client
+// purpose of this file is to create a new ApolloClient instance and export it so that we can use it in our app
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: graphqlUri,
+    uri: "http://localhost:4000/graphql", // this is the endpoint that we are making requests to
   }),
   cache: new InMemoryCache(),
 });
