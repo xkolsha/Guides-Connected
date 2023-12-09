7d841bb87a3039afb5ffc9a681d9c22ddda09d5d
xkolsha <aviad85@outlook.com>
2023-12-09 03:08:20 -0400

    update REACT_APP_GRAPHQL_ENDPOINT in apollo

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,12 +1,9 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-// purpose of this file is to create a new ApolloClient instance and export it so that we can use it in our app
+// Create the client as outlined in the Apollo docs
 const client = new ApolloClient({
   link: new HttpLink({
-    uri:
-      process.env.NODE_ENV === "production"
-        ? "https://guides-connected-5d7602b52f90.herokuapp.com/graphql"
-        : "http://localhost:4000/graphql",
+    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
   }),
   cache: new InMemoryCache(),
 });
