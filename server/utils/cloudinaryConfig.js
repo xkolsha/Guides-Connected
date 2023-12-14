11bb40e54f23bfe72f5f78587499ebed34cf9f05
xkolsha <aviad85@outlook.com>
2023-12-14 10:17:46 -0400

    add cloudinary

--- server/utils/cloudinaryConfig.js
+++ server/utils/cloudinaryConfig.js
@@ -1,9 +1,10 @@
 import cloudinary from "cloudinary";
 
+// Configure Cloudinary with environment variables
 cloudinary.config({
-  cloud_name: "YOUR_CLOUD_NAME",
-  api_key: "YOUR_API_KEY",
-  api_secret: "YOUR_API_SECRET",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
