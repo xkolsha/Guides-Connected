b05e243bd7e133d8dcdb1e31ee000bf54dba3145
xkolsha <aviad85@outlook.com>
2024-01-12 11:01:40 -0400

    test port

--- server/utils/mail.js
+++ server/utils/mail.js
@@ -4,15 +4,16 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
+
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USERNAME, // Your Gmail address
     pass: process.env.EMAIL_APP_PASSWORD, // App password you generated
   },
 });
 
-// Send email function remains the same
+// Send email function
 const sendEmail = async ({ name, email, message }) => {
   // define email options
   const mailOptions = {
